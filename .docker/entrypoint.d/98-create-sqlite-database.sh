2485dce5f8e42b16c1c07a5318f5c32a31716c64
Ruben Steen <rubensteen123@gmail.com>
2026-01-17 22:42:16 +0100

    Boom!

--- .docker/entrypoint.d/98-create-sqlite-database.sh
+++ .docker/entrypoint.d/98-create-sqlite-database.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-# Check if database.sqlite exists in the database folder
-if [ ! -f "$APP_BASE_DIR/database/database.sqlite" ]; then
+# Check if database.sqlite exists in the storage/database folder
+if [ ! -f "$APP_BASE_DIR/storage/database/database.sqlite" ]; then
   echo "ℹ️  Creating database.sqlite file..."
-  touch "$APP_BASE_DIR/database/database.sqlite"
+  touch "$APP_BASE_DIR/storage/database/database.sqlite"
   echo "✅ database.sqlite created successfully"
 else
   echo "ℹ️  database.sqlite already exists"
